2ca8c1db50a552f115eb2abd2b4e57ce44875bb2
Se7en Oussama <seven.tdr7@gmail.com>
2020-06-23 15:21:51 +0100

    complete contact.js with its style

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 
 class Contact extends Component {
 
     render() {
         return(
-            <div>
-                <h1>Contact Page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2> Bendifallah Oussama </h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{height: '300px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '2em'}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamilly: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (+33) 06-22-33-44-55
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamilly: 'Anton'}}>
+                                        <i className="fa fa-fax" aria-hidden="true"/>
+                                        (+33) 06-22-33-44-55
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamilly: 'Anton'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        ouss@yopmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamilly: 'Anton'}}>
+                                        <i className="fa fa-skype" aria-hidden="true"/>
+                                        Skype Id
+                                    </ListItemContent>
+                                </ListItem>
+                            </List> 
+                        </div>
+
+                    </Cell>
+                </Grid>
             </div>
         )
     }
--- src/components/projects.js
+++ src/components/projects.js
@@ -11,7 +11,9 @@ class Projects extends Component {
     toggleCategories() {
         if(this.state.activeTab === 0) {
             return(
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <div className="projects-grid">
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', marginRight: '30px'}}>
                         <CardTitle style={{color: 'black', height: '276px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png) center / cover'}}>
                             React Project #1
                         </CardTitle>
@@ -28,6 +30,43 @@ class Projects extends Component {
                             <IconButton name="share"/>
                         </CardMenu>
                 </Card>
+                {/* Project 2 */}
+                <Card shadow={5} style={{minWidth: '450', marginRight: '30px'}}>
+                        <CardTitle style={{color: 'black', height: '276px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png) center / cover'}}>
+                            React Project #1
+                        </CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored> GitHub </Button>
+                            <Button colored> Productions </Button>
+                            <Button colored> GitHub </Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+                </Card>
+                {/* Project 3 */}
+                <Card shadow={5} style={{minWidth: '450', marginRight: '30px'}}>
+                        <CardTitle style={{color: 'black', height: '276px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png) center / cover'}}>
+                            React Project #1
+                        </CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored> GitHub </Button>
+                            <Button colored> Productions </Button>
+                            <Button colored> GitHub </Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+                </Card>
+                </div>
             )
         } else if(this.state.activeTab === 1) {
             return (
@@ -44,13 +83,13 @@ class Projects extends Component {
         return(
             <div className="category-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>React </Tab>
                     <Tab>Ruby on Rails </Tab>
-                    <Tab>Reactjs </Tab>
                     <Tab>Bootstrap </Tab>
                 </Tabs>
 
                 <section className="projects-grid">
-                    <Grid className="projects-grid">
+                    <Grid>
                         <Cell col={12}>
                             <div className="content">
                                 {this.toggleCategories()}
